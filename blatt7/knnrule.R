310937338374faddb5bc48928cb92265c47954d3
Ingo Schaefer <ischonger@gmail.com>
2017-06-11 17:17:43 +0200

    final commit on blatt7/

--- blatt7/knnrule.R
+++ blatt7/knnrule.R
@@ -1,3 +1,4 @@
+# Ausführung dauert lange!! => einzeln ausführen
 # 2
 library(class)
 ?knn
@@ -13,13 +14,7 @@ knn.heldout <- function(train, test, k=1) {
 }
 
 # b)
-test <- function(data, k=1) {
-  lapply(data, function(x) { knn.heldout(data[-x], data[x], k=k)} )
-}
-
 knn.leave1out <- function(data, k=1) {
-  #sapply(data, function(x) knn.heldout(data[-x,],data[x,]), k=k)
-  
   s <- 0
   for(i in 1:dim(data)[1]) {
     s <- s + knn.heldout(data[-i,], data[i,],k=k)
@@ -58,29 +53,32 @@ m
 
 # g)
 #load(./path/to/file/letter.rda)
-g <- integer(12)
-for(e in 0:13) {
-  g[e] <- knn.heldout(letter.lern[1:(2^(e)),], letter.test) 
+g <- matrix(ncol=14, nrow=2)
+rownames(g) <- c("errors", "enumeration")
+colnames(g) <- c("e=2^0", "e=2^1", "e=2^2", "e=2^3", "e=2^4", "e=2^5", "e=2^6", "e=2^7", "e=2^8", "e=2^9", "e=2^10", "e=2^11", "e=2^12", "e=2^13")
+for(e in 1:14) {
+  g[1,e] <- knn.heldout(letter.lern[1:(2^(e-1)),], letter.test) * 100
+  g[2,e] <- e
 }
 g
 
 # h)
 total <- rbind(germany.lern, germany.test)
-h <- integer(24)
-h2 <- integer(24)
+germanyErrors <- matrix(ncol = 24, nrow = 3)
+colnames(germanyErrors) <- colnames(germanyErrors, do.NULL = FALSE, prefix = "fehlt ")
+rownames(germanyErrors) <- c("1-nn", "cross-val", "1-nn/c-v")
 for(i in 1:dim(total)[2]) {
-  h[i] <- knn.leave1out(total[,-i]) 
-  h2[i] <- knn.heldout(germany.lern[,-i], germany.test[,-i]) 
+  germanyErrors[1,i] <- knn.heldout(germany.lern[,-i], germany.test[,-i])
+  germanyErrors[2,i] <- knn.leave1out(total[,-i]) 
+  germanyErrors[3,i] <- germanyErrors[1,i] / germanyErrors[2,i] - 1
 }
-h; h2
+germanyErrors
 
 # i) (nur zur besseren Darstellung)
 par(mfrow=c(2,1));
 plot(h*100, main = "leave1out", xlab = "alle Merkmale außer i-tem", ylab = "Testfehler (%)" )
 plot(h2*100, main = "heldout", xlab = "alle Merkmale außer i-tem", ylab = "Testfehler (%)" )
 
-
-# 
 #j <- integer(10)
 #for(i in 1:10) {       #Berechnung dauert länger, daher ausgeklammert
 #  all <- knn.leave1out(total)
@@ -88,3 +86,8 @@ plot(h2*100, main = "heldout", xlab = "alle Merkmale außer i-tem", ylab = "Testf
 #  j[i] <- all-best23
 #}
 #j
+
+# Baue errors.rda
+#cdef <- m
+#h <- germanyErrors
+#save(cdef, g, h, file = "errors.rda")
